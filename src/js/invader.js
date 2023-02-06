0c3f20451d8c273345622532f10fabf12a0a782f
Bouard Bastien <bastien.bouard27@gmail.com>
2023-02-06 13:45:40 +0100

    tir sans vaisseau

--- src/js/invader.js
+++ src/js/invader.js
@@ -4,10 +4,10 @@ function grille() {
     for (var i = 0; i < 400; i++) {
       var div = document.createElement("div");
       parent.appendChild(div);
-      if (i % 20 === 0) { // Tous les multiple de 20
+      if (i % 20 === 0) { 
         div.className = "dataLeft";
       }
-      if (i % 20-19 === 0) { // Tous les multiple de 20-19 pour que ça aille de lautre coté
+      if (i % 20-19 === 0) { 
         div.className = "dataRight";
       }
     }
@@ -15,7 +15,32 @@ function grille() {
     
 }
 
+///////////////////////////////////// Tir du vaisseau ///////////////////////////////////////////////
 
-
-
-
+function tirer() {
+    var parent = document.getElementById("grille");
+    var div = document.createElement("div");
+    div.className = "laser";
+    div.style.top = "500px"; // Position initiale du tir
+    div.style.left = "765px"; // Position initiale du tir
+    parent.appendChild(div);
+    var vitesse = setInterval(function() {
+      var top = parseInt(div.style.top);
+      top -= 10; // Déplacement du tir vers le haut
+      div.style.top = top + "px";
+      if (top <= 90) {
+        clearInterval(vitesse); // Arrêt de l'animation lorsque le tir sort de la grille
+        parent.removeChild(div); // Suppression du tir du DOM
+      }
+    }, 30); 
+  }
+  
+  
+  
+document.addEventListener("keydown", function(event) {
+  if (event.code === "Space") {
+    tirer();
+  }
+});
+  
+//////////////////////////////////////////////////////////////////////////////////////////////
