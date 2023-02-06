586389eace97e191a7c9a7691e73c04f2fc3d007
Floria <florian.richet2004@gmail.com>
2023-02-06 15:13:00 +0100

    ennemie qui bouge que coter droite qui marche arrivé au bord

--- src/js/invader.js
+++ src/js/invader.js
@@ -1,21 +1,158 @@
 window.onload = grille();
 function grille() {
     var parent = document.getElementById("grille");
+    var listParent = document.getElementById("list");
+    var liste = [];
+
     for (var i = 0; i < 400; i++) {
       var div = document.createElement("div");
       parent.appendChild(div);
       if (i % 20 === 0) { // Tous les multiple de 20
         div.className = "dataLeft";
+        liste.push("dataLeft");
       }
-      if (i % 20-19 === 0) { // Tous les multiple de 20-19 pour que ça aille de lautre coté
+      else if (i % 20 === 19) { // Tous les multiple de 20-19 pour que ça aille de l'autre coté
         div.className = "dataRight";
+        liste.push("dataRight");
       }
+      else if (i === 10 ){
+        div.className = "alien";
+        div.classList.remove("cases6");
+        liste.push("alien");
+      }
+
+      else {
+        div.className = "cases"+i;
+        liste.push("cases"+i);
+      }
+
     }
 
+    console.log(liste);
+
+    var coterDroit = true;
+    var coterGauche = false;
+
+    setInterval(function() {
+        if (coterDroit == true){
+            console.log("----->");;
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index++;
+            console.log(liste[index]);
+            liste[index-1] = "cases" + (index-1);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".cases" + index);
+            nextAlien.classList.add("alien");
+        }
+        else if (coterGauche == true){
+            console.log("<-----");
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index=index-1;
+            console.log(liste[index]);
+            liste[index-1] = "cases" + (index-1);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".cases" + index);
+            nextAlien.classList.add("alien");
+        } else {
+            console.log("ok");
+        }
+        if (liste[index+1] == "dataRight"){
+            console.log("Coter Droit");
+            console.log(coterDroit);
+
+
+            console.log("----->");;
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index++;
+            console.log(liste[index]);
+            liste[index-1] = "cases" + (index-1);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".dataRight");
+            nextAlien.classList.add("alien");
+
+            console.log("↓");;
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index=index+20;
+            console.log(liste[index]);
+            liste[index-20] = "cases" + (index-20);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".dataRight");
+            nextAlien.classList.add("alien");
+
+            coterDroit = false;
+            coterGauche = true;
+        }
+        else if (liste[index-1] == "dataLeft"){
+            console.log("Coter Gauche");
+            console.log(coterGauche);
+
+
+            console.log("<-----");;
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index--;
+            console.log(liste[index]);
+            liste[index+1] = "cases" + (index+1);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".dataLeft");
+            nextAlien.classList.add("alien");
+
+            console.log("↓");;
+            var div = document.querySelector(".alien");
+            div.classList.remove("alien");
+            var index = liste.indexOf("alien");
+            index=index+20;
+            console.log(liste[index]);
+            liste[index-20] = "cases" + (index-20);
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".dataLeft");
+            nextAlien.classList.add("alien");
+
+            coterDroit = true;
+            coterGauche = false;
+        }
+
+
     
+    }, 1000);
+    
+
 }
 
 
 
 
+/*
+    setInterval(function() {
+        var div = document.querySelector(".alien");
+        div.classList.remove("alien");
+        var index = liste.indexOf("alien");
+        index++;
+        console.log(liste[index]);
+        if (liste[index] == "dataRight"){
+            console.log("sur dataRight");
+            index=index+20;
+            liste[index-20] = "dataRight";
+            liste[index] = "alien";
+            var nextAlien = document.querySelector(".dataRight");
+            nextAlien.classList.add("alien");
+            console.log(liste);
+            index=index-5;
+        }
+        liste[index-1] = "cases" + (index-1);
+        liste[index] = "alien";
+        var nextAlien = document.querySelector(".cases" + index);
+        nextAlien.classList.add("alien");
 
+    
+    }, 1000);
+    */
