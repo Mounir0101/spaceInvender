a09e847e8637ef87696bbdb5fdfbdda405ca74b2
mounir <mounirxchitour@gmail.com>
2023-02-06 16:25:57 +0100

    space invender

--- src/js/invader.js
+++ src/js/invader.js
@@ -1,7 +1,7 @@
 window.onload = grille();
 function grille() {
     var parent = document.getElementById("grille");
-    for (var i = 0; i < 400; i++) {
+    for (var i = 0; i < 240; i++) {
       var div = document.createElement("div");
       parent.appendChild(div);
       if (i % 20 === 0) { // Tous les multiple de 20
@@ -12,7 +12,50 @@ function grille() {
       }
     }
 
+
+
+    var divs = document.querySelectorAll('div');
+    var positiontireur = 230;
+    divs [positiontireur].classList.add('tireur') ;
     
+
+ 
+
+    let tireur = document.querySelector('tireur')
+
+    document.onkeydown = function (e) {
+        if (e.key == 'ArrowUp') {
+            for(let j = 0; j < 20; j++) {
+    
+                    let cases = document.querySelector('.tireur')
+            
+                            cases.classList.remove('tireur')
+                            cases.previousElementSibling.classList.add('tireur')                
+            }
+        }
+        if (e.key == 'ArrowDown') {
+            for(let j = 0; j < 20; j++) {
+                
+                    let cases = document.querySelector('.tireur')
+            
+                            cases.classList.remove('tireur')
+                            cases.nextElementSibling.classList.add('tireur')                
+            }
+        }
+        
+        if (e.key == 'ArrowLeft') {
+          var cases = document.querySelector('.tireur')
+           
+                cases.classList.remove('tireur')
+                cases.previousElementSibling.classList.add('tireur')
+            
+        }
+        var cases = document.querySelector('.tireur')
+        if (e.key == 'ArrowRight'){
+            cases.classList.remove('tireur')
+            cases.nextElementSibling.classList.add('tireur')
+        }
+    };
 }
 
 
